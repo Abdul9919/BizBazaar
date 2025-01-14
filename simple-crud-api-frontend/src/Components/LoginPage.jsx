2c3eaa319f7835b56c87fc5eb2d656097d20af42
Abdul9919 <parky2552@gmail.com>
2025-01-14 17:43:45 +0500

    completly revamped the login page replacing bootstrap with tailwind

--- simple-crud-api-frontend/src/Components/LoginPage.jsx
+++ simple-crud-api-frontend/src/Components/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
+import icon from "../Assets/favicon.png";
 
 const LoginPage = () => {
     const [form, setForm] = useState({
@@ -35,36 +36,63 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="container mt-5">
-            <h2>Login</h2>
-            {error && <div className="alert alert-danger">{error}</div>}
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        name="email"
-                        value={form.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        name="password"
-                        value={form.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                {/*<img
+                    className="mx-auto h-10 w-auto"
+                    src={icon}
+                    alt="Your Company"
+                />*/}
+
+                <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                {error && <div className="mb-4 text-red-500 text-center">{error}</div>}
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-900">Email address</label>
+                        <div className="mt-2">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={form.email}
+                                onChange={handleChange}
+                                required
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-900">Password</label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={form.password}
+                                onChange={handleChange}
+                                required
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-slate-700 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-stone-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transform transition-all duration-300 ease-in-out"
+                        >
+                            Sign in
+                        </button>
+
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
