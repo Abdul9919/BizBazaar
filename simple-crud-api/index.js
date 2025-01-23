fae721be2c95207987ecadb86426da57a0020d3f
Abdul9919 <parky2552@gmail.com>
2025-01-23 17:24:19 +0500

    minor bug fix

--- simple-crud-api/index.js
+++ simple-crud-api/index.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // Replace with your frontend URL
+  origin: 'http://localhost:3000', // Replace with your frontend URL
 }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
